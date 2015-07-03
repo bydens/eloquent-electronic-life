034be05afed39722f39cb0fe83fbde40240debcc
Denys Bykanov <denys.bykanov@globallogic.com>
2015-07-03 13:21:20 +0300

    Rename Wall object

---
+++ js/ecosystem/Wall.js
@@ -0,0 +1,9 @@
+/**
+ * @license eLife 1.0 Copyright (c) 2015, Denys Bykanov All Rights Reserved.
+ * Available via the MIT or new BSD license.
+ * see: https://github.com/bydens/eloquent-electronic-life for details
+ */
+
+  function Wall() {}
+  
+  module.exports = Wall;
